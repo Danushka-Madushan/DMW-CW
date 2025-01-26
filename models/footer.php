a809105d2ce76c373b999f3db9b6499f2de443cc
Danushka-Madushan <danushkamadushan128@gmail.com>
2025-01-27 00:08:06 +0530

    update footer.php

--- models/footer.php
+++ models/footer.php
@@ -3,8 +3,8 @@
     <div class="container" style="font-family: 'Roboto'; font-weight: 300">
         <p>&copy; <?php echo date("Y"); ?> DMW Project Group 22 (DSE)</p>
         <p>
-            <a href="#" class="text-light mx-2">Privacy Policy</a> | 
-            <a href="#" class="text-light mx-2">Terms of Service</a> | 
+            <a href="#" class="text-light mx-2">Privacy Policy</a> |
+            <a href="#" class="text-light mx-2">Terms of Service</a> |
             <a href="contact.php" class="text-light mx-2">Contact Us</a>
         </p>
     </div>
@@ -12,4 +12,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
+
 </html>
