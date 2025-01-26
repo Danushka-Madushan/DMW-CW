c4be93858f0a7b2d7281b47fea450096205f123e
Danushka-Madushan <danushkamadushan128@gmail.com>
2025-01-27 00:07:48 +0530

    update cart.php

--- cart.php
+++ cart.php
@@ -6,6 +6,9 @@ include 'models/header.php';
 if (!isset($_SESSION['user_id'])) {
     die('<script>location.replace("login.php");</script>');
 }
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = [];
+}
 ?>
 <section class="h-100">
     <div class="container h-100 py-3">
@@ -18,76 +21,75 @@ if (!isset($_SESSION['user_id'])) {
                         <small class="display-6">Details</small>
                     </h3>
                 </div>
+                <?php
+                include 'models/db.php';
+                include 'models/utils.php';
+                include 'models/cartfunc.php';
 
-                <div class="card rounded-3 mb-4">
-                    <div class="card-body px-4 py-2">
-                        <div class="row d-flex justify-content-between align-items-center">
-                            <div class="col-md-2 col-lg-2 col-xl-2">
-                                <img src="https://cdn.sanity.io/images/yqd1zell/production/9559d692d06ac1356e3eb54446c6ba858f156a90-512x512.png"
-                                    class="img-fluid rounded-3" alt="Cotton T-shirt">
-                            </div>
-                            <div class="col-md-3 col-lg-3 col-xl-3">
-                                <p class="lead fw-normal mb-2">Asus Prime A520M-E DDR4 Motherboard</p>
-                                <p><span class="text-muted">Qty: </span>1 <span class="text-muted">Item Price: </span>Rs 25000
-                                </p>
-                            </div>
-                            <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                    onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                    <i class="fas fa-minus"></i>
-                                </button>
+                if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['delete_id'])) {
+                    removeFromCart($_POST['delete_id']);
+                }
 
-                                <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                    onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                    <i class="fas fa-plus"></i>
-                                </button>
-                            </div>
-                            <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                <h5 class="mb-0">Rs 25000.00</h5>
-                            </div>
-                            <div class="col-md-1 col-lg-1 col-xl-1 text-end" style="margin-right: 30px;">
-                                <a href="#!" class="text-danger" style="font-size: 20px;"><i
-                                        class="bi bi-trash-fill"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                if (count($_SESSION['cart']) === 0) {
+                    echo "<div class='container d-flex flex-column align-items-center justify-content-center'>";
+                    echo "<div class='card d-flex  text-center p-4 border-0 shadow-sm align-items-center justify-content-center'>";
+                    echo "<img src='https://cdn-icons-png.flaticon.com/512/743/743131.png' alt='Empty Box' class='img-fluid mb-3' style='width: 150px;'>";
+                    echo "<h5 class='fw-bold'>No Orders Yet</h5>";
+                    echo "<p class='text-muted'>Looks like you haven't placed any orders yet. Start shopping now!</p>";
+                    echo "<a href='./products.php' class='btn btn-primary'>Shop Now</a></div></div>";
+                } else {
+                    $total_amount = 0;
 
-                <div class="col-md-4">
-                    <div class="card mb-4">
-                        <div class="card-header py-3">
-                            <h5 class="mb-0">Summary</h5>
-                        </div>
-                        <div class="card-body">
-                            <ul class="list-group list-group-flush">
-                                <li
-                                    class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                                    Products
-                                    <span>$53.98</span>
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-center px-0">
-                                    Shipping
-                                    <span>Rs 1500.00</span>
-                                </li>
-                                <li
-                                    class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
-                                    <div>
-                                        <h5 class="mb-0">Total Amount</h5>
-                                    </div>
-                                    <h5 class="mb-0">$499.00</h5>
-                                </li>
-                            </ul>
+                    foreach ($_SESSION['cart'] as &$item) {
+                        $stmt = $conn->prepare("SELECT * FROM products WHERE product_id = ?");
+                        $stmt->execute([$item['product_id']]);
 
-                            <button type="button" data-mdb-button-init data-mdb-ripple-init
-                                class="btn btn-primary btn-lg btn-block">
-                                Proceed Payment
-                            </button>
-                        </div>
-                    </div>
-                </div>
+                        // Fetch the product data
+                        $product = $stmt->fetch(PDO::FETCH_ASSOC);
 
+                        if ($product) {
+                            $price_ = (int) $item['amount'] * (int) $product['price'];
+                            $total_amount += $price_;
+                            echo "<div class='card rounded-3 mb-4'>";
+                            echo "<div class='card-body px-4 py-2'>";
+                            echo "<div class='row d-flex justify-content-between align-items-center'>";
+                            echo "<div class='col-md-2 col-lg-2 col-xl-2'>";
+                            echo "<img src='{$product['image']}' class='img-fluid rounded-3' alt='Cotton T-shirt'>";
+                            echo "</div><div class='col-md-3 col-lg-3 col-xl-3'>";
+                            echo "<p class='lead fw-normal mb-2'>{$product['name']}</p>";
+                            echo "<p><span class='text-muted'>Qty: </span>{$item['amount']} <span class='text-muted'>Item Price: </span>Rs " . (int) $product['price'];
+                            echo "</p></div><div class='col-md-3 col-lg-3 col-xl-2 d-flex'>";
+                            echo "<button data-mdb-button-init data-mdb-ripple-init class='btn btn-link px-2' onclick='this.parentNode.querySelector(\"input[type=number]\").stepDown()'>";
+                            echo "<i class='fas fa-minus'></i></button>";
+                            echo "<button data-mdb-button-init data-mdb-ripple-init class='btn btn-link px-2' onclick='this.parentNode.querySelector(\"input[type=number]\").stepUp()'>";
+                            echo "<i class='fas fa-plus'></i></button>";
+                            echo "</div><div class='col-md-3 col-lg-2 col-xl-2 offset-lg-1'>";
+                            echo "<h5 class='mb-0'>Rs {$price_}.00</h5></div>";
+                            echo "<form method='POST' ty class='col-md-1 col-lg-1 col-xl-1 text-end' style='margin-right: 30px;'>";
+                            echo "<input type='hidden' name='delete_id' value='{$item['product_id']}'>";
+                            echo "<button type='submit' class='btn text-danger' style='font-size: 20px;'><i class='bi bi-trash-fill'></i></button>";
+                            echo "</form></div></div></div>";
+                        }
+                    }
+
+                    $delivery_charge = 300 * count($_SESSION['cart']);
+
+                    echo "<div class='col-md-4'><div class='card mb-4'><div class='card-header py-3'>";
+                    echo "<h5 class='mb-0'>Summary</h5>";
+                    echo "</div><div class='card-body'><ul class='list-group list-group-flush'>";
+                    echo "<li class='list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0'>Products<span>Rs " . customFormatPrice($total_amount) . "</span>";
+                    echo "</li><li class='list-group-item d-flex justify-content-between align-items-center px-0'>Shipping";
+                    echo "<span>Rs {$delivery_charge}.00</span></li><li class='list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3'>";
+                    echo "<div><h5 class='mb-0'>Total Amount</h5></div><h5 class='mb-0'>Rs " . customFormatPrice((int) $delivery_charge + (int) $total_amount) . "</h5></li></ul>";
+                    echo "<form action='payment.php' method='POST'>";
+                    echo "<input type='hidden' name='delcharge' value='{$delivery_charge}'>";
+                    echo "<button type='submit' data-mdb-button-init data-mdb-ripple-init class='btn btn-primary btn-md btn-block'>Proceed Payment</button></form>";
+                    echo "</div></div></div>";
+                }
+                ?>
             </div>
         </div>
     </div>
 </section>
-<?php include 'models/footer.php'; ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
