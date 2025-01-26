84999b58896eb8712b5f9db46ff08ed2d3b1e89b
Danushka-Madushan <danushkamadushan128@gmail.com>
2025-01-27 00:25:49 +0530

    update db.php

--- models/db.php
+++ models/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost"; // This is the container name, not localhost
 $dbname = "trustcareComputers";
-$username = "root";
-$password = "";
+$username = "webapp";
+$password = "BrsMVUDvx3BHfpN4kt8I";
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
