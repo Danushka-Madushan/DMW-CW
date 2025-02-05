a4ebc48cf889368079861d40e0928895251d98e5
Danushka-Madushan <danushkamadushan128@gmail.com>
2025-02-05 22:43:11 +0530

    refactor: enhance header styling and improve product display layout in index.php

--- index.php
+++ index.php
@@ -5,11 +5,12 @@ include 'models/db.php';
 
 <body>
     <!-- Header-->
-    <header class="bg-dark py-5">
-        <div class="container px-4 px-lg-5 my-5">
+    <header class="bg-dark">
+        <div
+            class="container-fluid bg-container text-center text-white d-flex align-items-center justify-content-center">
             <div class="text-center text-white">
-                <h1 class="display-5 fw-bolder">Dominate with the BEST</h1>
-                <p class="lead fw-normal text-white-50 mb-0">Custom PCs with World-class Hadware</p>
+                <h1 class="display-5 fw-bolder glow-text-blue">DOMINATE WITH THE BEST</h1>
+                <p class="lead fw-normal text-white-50 mb-0 glow-text-blue">CUSTOM PC'S WITH WORLD CLASS HARDWARE</p>
             </div>
         </div>
     </header>
@@ -17,38 +18,38 @@ include 'models/db.php';
     <section class="pb-5 pt-3">
         <div class="container px-4 px-lg-5 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-            <?php
-            $sql = "SELECT * FROM products WHERE category IN ('Processors', 'Motherboards', 'Coolers', 'SSD', 'Graphics Cards', 'Monitors', 'Laptops', 'Apple Products') ORDER BY RAND() LIMIT 8;";
-            $products = $conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                <?php
+                $sql = "SELECT * FROM products WHERE category IN ('Processors', 'Motherboards', 'Coolers', 'SSD', 'Graphics Cards', 'Monitors', 'Laptops', 'Apple Products') ORDER BY RAND() LIMIT 8;";
+                $products = $conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-            foreach ($products as $product) {
-                echo "<div class='col mb-5'>";
-                echo "<div class='card h-100'>";
-                echo "<div class='badge bg-success rounded-pill text-white position-absolute' style='top: 0.5rem; right: 0.5rem'>New Arrival</div>";
-                echo "<img class='card-img-top' src='{$product['image']}' alt='...' />";
-                echo "<div class='card-body px-4 pt-2'>";
-                echo "<div class='text-center'>";
-                echo "<h5 class='fw-bolder'>{$product['name']}</h5>";
+                foreach ($products as $product) {
+                    echo "<div class='col mb-5'>";
+                    echo "<div class='card h-100'>";
+                    echo "<div class='badge bg-success rounded-pill text-white position-absolute' style='top: 0.5rem; right: 0.5rem'>New Arrival</div>";
+                    echo "<img class='card-img-top' src='{$product['image']}' alt='...' />";
+                    echo "<div class='card-body px-4 pt-2'>";
+                    echo "<div class='text-center'>";
+                    echo "<h5 class='fw-bolder'>{$product['name']}</h5>";
 
-                echo "</div></div>";
+                    echo "</div></div>";
 
-                echo "<div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>";
-                
-                echo "<div class='d-flex justify-content-center small text-warning mb-2'>";
-                /* Dynamic Rating Start based on product rating */
-                for ($x = 0; $x < (int)$product['rating']; $x++) {
-                    echo "<div class='bi-star-fill'></div>";
+                    echo "<div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>";
+
+                    echo "<div class='d-flex justify-content-center small text-warning mb-2'>";
+                    /* Dynamic Rating Start based on product rating */
+                    for ($x = 0; $x < (int) $product['rating']; $x++) {
+                        echo "<div class='bi-star-fill'></div>";
+                    }
+                    for ($x = 0; $x < (5 - (int) $product['rating']); $x++) {
+                        echo "<div class='bi-star'></div>";
+                    }
+                    echo "</div>";
+                    echo "<div class='text-center mb-2 lead' style='font-family: Roboto'>Rs {$product['price']}</div>";
+
+                    echo "<div class='text-center'><a class='btn btn-outline-primary mt-auto' href='./view_product.php?id={$product['product_id']}'>View</a>";
+                    echo "</div></div></div></div>";
                 }
-                for ($x = 0; $x < (5 - (int)$product['rating']); $x++) {
-                    echo "<div class='bi-star'></div>";
-                }
-                echo "</div>";
-                echo "<div class='text-center mb-2 lead' style='font-family: Roboto'>Rs {$product['price']}</div>";
-
-                echo "<div class='text-center'><a class='btn btn-outline-primary mt-auto' href='./view_product.php?id={$product['product_id']}'>View</a>";
-                echo "</div></div></div></div>";
-            }
-            ?>
+                ?>
             </div>
         </div>
     </section>
